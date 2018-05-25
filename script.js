bafd3182bccd3fdc79d7f8f9e0418ad66dac48b7
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2018-05-25 13:43:10 -0400

    changing image format

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var CorrectMoves = 0;
+vbcb  var CorrectMoves = 0;
 function ClickHandler(e)
 {
 	//alert("Clicked a button");
 	var element = $(this);
 	element.css("background-color", "transparent");
-		if(element.next("h1").text() === "Bomb"){
+		if(element.next("h1").class() === "bomb"){
 			$("button").css("background-color", "transparent");
 			alert("Kaboom!");
 			
 		}
-		else if (CorrectMoves === 12)
+		else if (CorrectMoves === 13)
 		{	
 			$("button").css("background-color", "transparent");
 			alert("YOU WON!");
